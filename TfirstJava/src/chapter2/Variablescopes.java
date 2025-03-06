6a812af611bb9a998a5674da799fb546f6ab1204
Yselam <yalewselam07@gmail.com>
2025-03-06 12:31:32 -0500

    Variable Scope had been modified!

--- TfirstJava/src/chapter2/Variablescopes.java
+++ TfirstJava/src/chapter2/Variablescopes.java
@@ -4,6 +4,8 @@ public class Variablescopes {
 	
 	int y = 20; 
 	static int z = 30;
+	
+	double S = 20876.99;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,7 +14,7 @@ public class Variablescopes {
 		
 		
 			
-			int x = 10;
+			int x = 2025;
 			 
 			System.out.println(x);
 			
@@ -20,7 +22,8 @@ public class Variablescopes {
 			Variablescopes mysub = new Variablescopes();
 			System.out.println(mysub.y);
 			
-			System.out.print(mysub.z);
+			System.out.println(mysub.z);
+			System.out.println(mysub.S);
 			
 		
 			
