4785cfb7846b833787d68711728f7b612d73b760
Hilina1993 <hgeremew1@gmail.com>
2025-03-06 21:03:42 -0500

    space

--- firstjava/src/firstjava11/vc_branching.java
+++ firstjava/src/firstjava11/vc_branching.java
@@ -5,7 +5,7 @@ public class vc_branching {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     String name =("Hilina");
-    System.out.println(name + " Abebe");
+    System.out.println( name + " Abebe");
     
 	}
 
