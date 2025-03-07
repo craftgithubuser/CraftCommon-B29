35c0b2e134c686f2e3c2e104b65538e85999e9c7
Hilina1993 <hgeremew1@gmail.com>
2025-03-06 20:54:24 -0500

    file

---
+++ javafile/src/javafile/Array.java
@@ -0,0 +1,26 @@
+package javafile;
+
+public class Array {
+
+	public static void main(String[] args) {
+	
+		int	[] array_number = {10,5,87,15,100};
+		System.out.println(array_number[0]);
+		System.out.println(array_number[1]);
+		System.out.println(array_number[2]);
+		System.out.println(array_number[3]);
+		System.out.println(array_number[4]);
+		
+		for (int i=0 ; i< array_number.length; i++) {
+		
+		
+		System.out.println(" index " + i + "-> " + array_number[i] );
+		
+	}
+		
+		
+		
+
+	}
+
+}
