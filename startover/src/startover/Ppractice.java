46a46464bf9fc6f39277b3f80e875b610f5d60af
binu47 <79720264+binu47@users.noreply.github.com>
2025-03-08 18:41:48 -0800

    added

---
+++ startover/src/startover/Ppractice.java
@@ -0,0 +1,24 @@
+package startover;
+
+public class Ppractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for(int i=1; i<10; i++){
+//            System.out.println("i =" + i);
+//            if (i % 2==0 && (i % 3==0)){
+//            System.out.println("Break at NUM")
+//                break;
+		
+		 for(int i=1; i<= 52; i++){
+	            if (i==32){
+	                System.out.println("An incorrect card was found in item 32.");
+	                break;
+	            }else {
+	            	System.out.println("card"+   i);
+	            }
+		 }
+
+	}
+
+}
